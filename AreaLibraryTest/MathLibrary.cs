5efd0729023212aa848b48357d45bc598e81eed5
Majd-Raad <masckmassck@gmail.com>
2022-05-04 13:42:45 +0300

    04.05.2022

---
+++ AreaLibraryTest/MathLibrary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaLibraryTest
+{
+    public class MathLibrary
+    {
+        public static double ComputeArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double ComputeArea(double FirstSide, double SecondSide, double ThirdSide)
+        {
+            if (Math.Pow(FirstSide, 2) == Math.Pow(SecondSide, 2) + Math.Pow(ThirdSide, 2))
+            {
+                return SecondSide * ThirdSide / 2;
+            }
+            else
+            {
+                if (Math.Pow(SecondSide, 2) == Math.Pow(FirstSide, 2) + Math.Pow(ThirdSide, 2))
+                {
+                    return FirstSide * ThirdSide / 2;
+                }
+                else
+                {
+                    if (Math.Pow(ThirdSide, 2) == Math.Pow(FirstSide, 2) + Math.Pow(SecondSide, 2))
+                    {
+                        return FirstSide * SecondSide / 2;
+                    }
+                    else
+                    {
+                        double HalfPerimeter = (FirstSide + SecondSide + ThirdSide) / 2;
+                        return Math.Sqrt((HalfPerimeter * (HalfPerimeter - FirstSide) * (HalfPerimeter - SecondSide) * (HalfPerimeter - ThirdSide)));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using AreaLibraryTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string res = "";
+            while (res != "0")
+            {
+                Console.WriteLine("Enter 0 to  Exit\nEnter C for Circle\nEnter T for Triangle");
+                res = Console.ReadLine();
+                switch(res.ToLower())
+                {
+                    case "c":
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter Radius");
+                                double radius = double.Parse(Console.ReadLine().Trim());
+                                Console.WriteLine("Area of Circle  "+MathLibrary.ComputeArea(radius));
+                            }
+                            catch
+                            {
+                                Console.WriteLine("ERROR");
+                            }
+                            break;
+                        }
+                    case "t":
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter First Side");
+                                double first = double.Parse(Console.ReadLine().Trim());
+                                Console.WriteLine("Enter Second Side");
+                                double second = double.Parse(Console.ReadLine().Trim());
+                                Console.WriteLine("Enter Third Side");
+                                double third = double.Parse(Console.ReadLine().Trim());
+                                Console.WriteLine("Area of Triangle  " + MathLibrary.ComputeArea(first, second, third));
+                            }
+                            catch
+                            {
+                                Console.WriteLine("ERROR");
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
